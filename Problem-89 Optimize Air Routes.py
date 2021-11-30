878379997a6eb19a5eef80dfda54952e8c487c27
Adhish Shrivastava <adhish70@gmail.com>
2021-11-30 13:15:19 -0500

    Initial commit

---
+++ Problem-89 Optimize Air Routes.py
@@ -0,0 +1,8 @@
+# Optimize Air Routes
+# https://leetcode.com/discuss/interview-question/373202
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
---
+++ Problem-90 H-Index II.py
@@ -0,0 +1,11 @@
+# 275. H-Index II
+# https://leetcode.com/problems/h-index-ii/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        pass
