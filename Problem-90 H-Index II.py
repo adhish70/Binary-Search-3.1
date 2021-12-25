53e061558e84ceb23eae0610eb9abd2e368aed2f
Adhish Shrivastava <adhish70@gmail.com>
2021-12-24 19:42:52 -0500

    Problem 90 done

--- Problem-90 H-Index II.py
+++ Problem-90 H-Index II.py
@@ -1,11 +1,24 @@
 # 275. H-Index II
 # https://leetcode.com/problems/h-index-ii/
 
-# Logic: 
-
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        pass
+        n = len(citations)
+        left = 0
+        right = n - 1
+        
+        while left <= right:
+            mid = left + (right-left)//2
+            
+            if citations[mid] == n - mid:
+                return n - mid
+            
+            if citations[mid] < n - mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+                    
+        return n - left
